e4ca5444d99bde3d9a17b5949445aa6e9f5a19e6
sohan1 <shn619@gmail.com>
2015-10-12 16:49:58 +0530

    chnages url

--- application/views/home.php
+++ application/views/home.php
@@ -19,7 +19,7 @@
                     <div class="img-overlay-solid" style="background-color:rgba(60,62,71,0.3);"></div>
                     <div class="caption text-center">
                         <div class="color-white text-center weight-300 large-caption " style="margin-top: -150px">Are you ready to be</div>
-                        <div class="logo-img"><img src="assets/img/logo.png" alt="VIIREALESTATE"></div>
+                        <div class="logo-img"><img src="<?php echo base_url("assets/img/logo.png");?>" alt="VIIREALESTATE"></div>
                         <div class="color-white text-center weight-800 super-large-caption vlogo">v<span class="two">II</span>RealEstate</div>
                         <h4 class="color-white text-center weight-600 uppercase " style="letter-spacing: 1px;">Find out all about us</h4>
                         <a href="#about" class="scrollto btn btn-primary btn-outline btn-lg rounded" style="margin-top: 10px"><i class="fa-chevron-down" style="font-size: 16px"></i></a>
