5654c40a07325c540a7e733dd88625566d2bb3af
Chintan <chintan@wohlig.com>
2015-12-04 12:18:53 +0530

    changes

--- application/views/frontend/contact-form.php
+++ application/views/frontend/contact-form.php
@@ -1,7 +1,7 @@
 <?php
 // ADD YOUR INFOMATION HERE
-$recipient = "v2re.contact@gmail.com";
-$successPage = "index.html";
+$recipient = 'v2re.contact@gmail.com';
+$successPage = 'index.html';
 
 // NO NEED TO EDIT ANYTHING BELOW THIS LINE =====================//
 
@@ -12,35 +12,32 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-
-$name=stripslashes($name);
-$email=stripslashes($email);
-$subject=stripslashes($subject);
-$message=stripslashes($message);
-$message= "Name: $name, \n\n Message: $message";
+$name = stripslashes($name);
+$email = stripslashes($email);
+$subject = stripslashes($subject);
+$message = stripslashes($message);
+$message = "Name: $name, \n\n Message: $message";
 
 /*
 Simple form validation
 check to see if an email and message were entered
 */
 //if no message entered and no email entered print an error
-if (empty($message) && empty($email)){
-print "No email address and no message was entered. <br>Please include an email and a message";
+if (empty($message) && empty($email)) {
+    print 'No email address and no message was entered. <br>Please include an email and a message';
 }
 //if no message entered send print an error
-elseif (empty($message)){
-print "No message was entered.<br>Please include a message.<br>";
+elseif (empty($message)) {
+    print 'No message was entered.<br>Please include a message.<br>';
 }
 //if no email entered send print an error
-elseif (empty($email)){
-print "No email address was entered.<br>Please include your email. <br>";
+elseif (empty($email)) {
+    print 'No email address was entered.<br>Please include your email. <br>';
 }
 //if the form has both an email and a message
 else {
 
 //mail the form contents
-mail( "$recipient", "$subject", "$message", "From: $email" );
-header("Location: $successPage");
+mail("$recipient", "$subject", "$message", "From: $email");
+    header("Location: $successPage");
 }
-
-?>
