c1cb704de9a0cf33b5b81dc57ab83e48adc4ea8e
Chintan <chintan@wohlig.com>
2015-12-04 11:03:43 +0530

    changes

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,7 +3,7 @@
 			<div class="row" style="margin-top: 20px">
 				<div class="text-center">
 					<div class="copy nomg">
-						Copyright © 2015 <a href="#">V2 Real Estate Pvt Ltd</a> All right are reserved
+						Copyright © <?php echo date("Y"); ?> <a href="#">V2 Real Estate Pvt Ltd</a> All right are reserved.
 					</div>
 				</div>
 			</div>
